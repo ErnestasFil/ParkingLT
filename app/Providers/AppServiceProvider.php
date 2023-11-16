fab4eca299ccfa8fd9a86396ef6f7a3b120b22e8
ErnestasFil <ernestas.filipavicius@ktu.edu>
2023-11-16 02:53:55 +0200

    Updates fro https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +26,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (App::environment("production")) {
+            URL::forceScheme("https");
+        }
         JsonResource::withoutWrapping();
     }
 }
